78d83287db56f8440de761ef2192a2796b6dde3d
Ygor Leon <107506941+Ygprleon@users.noreply.github.com>
2023-11-29 22:57:59 -0300

    Agregar archivos e imagenes

---
+++ js/jscript.js
@@ -0,0 +1,8 @@
+const productos = [
+    {id: 1, nombre: "HarinaPan", precio: 1000, img: imagenes/HarinaPan.jpeg, cantidad: 1},
+    {id: 2, nombre: "Chicha", precio: 1500, img: imagenes/Chicha.jpeg, cantidad: 1},
+    {id: 3, nombre: "Cachapas", precio: 1200, img: imagenes/Cachapa.jpeg, cantidad: 1},
+    {id: 4, nombre: "Tequeños", precio: 800, img: imagenes/Tequeños.jpeg, cantidad: 1},
+    ];
+
+
